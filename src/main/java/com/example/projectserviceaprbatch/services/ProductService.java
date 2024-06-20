24a643bea74fdeacc0c3b5e8a7825da0187ff25a
neetagupta23 <neetuvrma@gmail.com>
2024-06-20 09:23:16 +0530

    post command Commits

--- src/main/java/com/example/projectserviceaprbatch/services/ProductService.java
+++ src/main/java/com/example/projectserviceaprbatch/services/ProductService.java
@@ -8,7 +8,7 @@ public interface ProductService {
     public Product getProductById(long id);
     public List<Product> getAllProducts();
     public Product updateProduct();
-    public Product createProduct();
+    public Product createProduct(Product product);
     public Product replaceProduct();
     public Product deleteProduct();
 
